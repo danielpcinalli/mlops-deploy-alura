8513e18c58666009836ad488abbd5f3f01a2f0e1
danielpcinalli <danielpcinalli@gmail.com>
2022-01-12 12:04:42 -0300

    alterações para App Engine

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
+from textblob import TextBlob
+import pickle
+import os
+
+colunas = ['tamanho', 'ano', 'garagem']
+modelo = pickle.load(open('models/modelo.sav', 'rb'))
+
+app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
+app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
+
+basic_auth = BasicAuth(app)
+
+
+@app.route('/')
+@basic_auth.required
+def home():
+    return "Minha primeira API."
+
+@app.route('/sentimento/<frase>')
+@basic_auth.required
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(to='en')
+    polaridade = tb_en.sentiment.polarity
+    return "polaridade: {}".format(polaridade)
+
+@app.route('/cotacao/', methods=['POST'])
+@basic_auth.required
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    preco = modelo.predict([dados_input])
+    return jsonify(preco=preco[0])
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
